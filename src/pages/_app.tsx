37ebfbfcb6fa2b8e3414f2e5eb7236803f94d895
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-06 23:01:22 -0300

    styles: app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app';
 import { globalStyles } from '../styles/global';
+import { Container, Header } from '../styles/pages/app';
+import logoImg from '../assets/logo.svg';
+import Image from 'next/image';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
